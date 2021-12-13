44dab5a8f3c40d23eb31ec710107f79b97bd91dd
Alberto Rodrigues <alberodrigues.filo@outlook.com>
2021-12-13 20:26:40 -0300

    Criacao do Projeto Carro - Exercicio Modulo 4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 
-
 public class Main {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Carro carro = new Carro();
         carro.setCor(carro.VERMELHO);
         carro.setMotor(carro.MIL);
