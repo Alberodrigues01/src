242e52a331bbceea6669fb2e48daf27f33bdf0d2
Alberto Rodrigues <alberodrigues.filo@outlook.com>
2021-12-16 12:12:57 -0300

    Projeto Carro - Revisao 1 Exercicio Modulo 4

--- src/Carro.java
+++ src/Carro.java
@@ -1,55 +1,25 @@
-import java.util.Scanner;
-
 public class Carro {
 
-    public static final String VERMELHO = "Vermelho";
-    public static final String MIL = "Mil";
-    public static final String ESPORTIVO = "Esportivo";
-    private Integer quantidadePortas;
-    private String numeroChassi;
-    private Integer anoFabricacao;
-    private String combustivel;
+    public Integer quantidadePortas;
+    public Integer anoFabricacao;
+    public String numeroChassi;
+    public String combustivel;
     private String cor;
-    private String motor;
+    private double motor;
     private String acabamento;
 
-
-    public Integer getQuantidadePortas(){
-        return quantidadePortas = 4;
-    }
-    public void setQuantidadePortas(Integer quantidadePortas){
-        this.quantidadePortas = quantidadePortas;
-    }
     public String getCor() {
         return cor;
     }
     public void setCor(String cor) {
         this.cor = cor;
     }
-
-    public String getNumeroChassi() {
-        return numeroChassi = "9BG116GW04C400001";}
-    public void setNumeroChassi(String numeroChassi) {
-        this.numeroChassi = numeroChassi;
-    }
-
-    public Integer getAnoFabricacao() {
-        return anoFabricacao = 2020;}
-    public void setAnoFabricacao(Integer anoFabricacao) {
-        this.anoFabricacao = anoFabricacao;
-    }
-    public String getCombustivel() {return combustivel = "FLEX"; }
-    public void setCombustivel(String combustivel) {
-        this.combustivel = combustivel;
-    }
-
-    public String getMotor() {
+    public double getMotor() {
         return motor;
     }
-    public void setMotor(String motor) {
+    public void setMotor(double motor) {
         this.motor = motor;
     }
-
     public String getAcabamento() {
         return acabamento;
     }
@@ -57,17 +27,15 @@ public class Carro {
         this.acabamento = acabamento;
     }
 
-     public void imprimeValores(){
-     System.out.println(" ======== CADASTRO DE VEÍCULOS ========");
-     System.out.println("Quantidade de Portas => " + getQuantidadePortas());
-     System.out.println("Número do Chassi => " + getNumeroChassi());
-     System.out.println("Ano de Fabricação  => " + getAnoFabricacao());
-     System.out.println("Tipo de Combustível => " + getCombustivel());
-     System.out.println("Cor => " + getCor());
-     System.out.println("Motor => " + getMotor());
-     System.out.println("Acabamento => " +getAcabamento());
-
-
+    public void imprimeValores(){
+     System.out.println("======== CADASTRO DE VEÍCULOS ========");
+     System.out.println("Quantidade de Portas => " + quantidadePortas);
+     System.out.println("Ano de Fabricação => " + anoFabricacao);
+     System.out.println("Número do Chassi  => " + numeroChassi);
+     System.out.println("Combustível       => " + combustivel);
+     System.out.println("Cor               => " + getCor());
+     System.out.println("Motor             => " + getMotor());
+     System.out.println("Acabamento        => " + getAcabamento());
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ public class Main {
 
     public static void main(String[] args) {
         Carro carro = new Carro();
-        carro.setCor(carro.VERMELHO);
-        carro.setMotor(carro.MIL);
-        carro.setAcabamento(carro.ESPORTIVO);
+        carro.quantidadePortas = 4;
+        carro.anoFabricacao = 2020;
+        carro.numeroChassi = "9BG116GW04C400001";
+        carro.combustivel = "FLEX";
+        carro.setCor("Vermelho");
+        carro.setMotor(1.4);
+        carro.setAcabamento("GLX");
         carro.imprimeValores();
     }
 }
