4d2056d47761eea8549cde879937cdc8dd76f010
Alberto Rodrigues <alberodrigues.filo@outlook.com>
2021-12-17 17:38:07 -0300

    Projeto Carro - Revisao 2 Exercicio Modulo 4

--- src/Carro.java
+++ src/Carro.java
@@ -1,41 +1,51 @@
 public class Carro {
 
-    public Integer quantidadePortas;
-    public Integer anoFabricacao;
-    public String numeroChassi;
-    public String combustivel;
-    private String cor;
-    private double motor;
-    private String acabamento;
+    private Integer quantidadePortas;
+    private Integer anoFabricaco;
+    private String numeroChassi;
+    private String combustivel;
+    public String cor;
+    public String motor;
+    public String acabamento;
 
-    public String getCor() {
+    public Carro(Integer quantidadePortas, Integer anoFabricacao, String numeroChassi, String combustivel){
+        this.quantidadePortas = quantidadePortas;
+        this.anoFabricaco = anoFabricacao;
+        this.numeroChassi = numeroChassi;
+        this.combustivel = combustivel;
+        }
+
+   public String getCor (){
         return cor;
-    }
+   }
     public void setCor(String cor) {
         this.cor = cor;
     }
-    public double getMotor() {
+
+    public String getMotor() {
         return motor;
     }
-    public void setMotor(double motor) {
+
+    public void setMotor(String motor) {
         this.motor = motor;
     }
+
     public String getAcabamento() {
         return acabamento;
     }
+
     public void setAcabamento(String acabamento) {
         this.acabamento = acabamento;
     }
 
     public void imprimeValores(){
-     System.out.println("======== CADASTRO DE VEÍCULOS ========");
-     System.out.println("Quantidade de Portas => " + quantidadePortas);
-     System.out.println("Ano de Fabricação => " + anoFabricacao);
-     System.out.println("Número do Chassi  => " + numeroChassi);
-     System.out.println("Combustível       => " + combustivel);
-     System.out.println("Cor               => " + getCor());
-     System.out.println("Motor             => " + getMotor());
-     System.out.println("Acabamento        => " + getAcabamento());
+            System.out.println("Quantidade de Portas => " + quantidadePortas);
+            System.out.println("Ano de Fabricacao => " + anoFabricaco);
+            System.out.println("Número do Chassi  => " + numeroChassi);
+            System.out.println("Combustível       => " + combustivel);
+            System.out.println("Cor               => " + getCor());
+            System.out.println("Motor             => " + getMotor());
+            System.out.println("Acabamento        => " + getAcabamento());
+        }
 
     }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-
 public class Main {
 
     public static void main(String[] args) {
-        Carro carro = new Carro();
-        carro.quantidadePortas = 4;
-        carro.anoFabricacao = 2020;
-        carro.numeroChassi = "9BG116GW04C400001";
-        carro.combustivel = "FLEX";
+        Carro carro = new Carro(4,2020,"9BG116GW04C40001",
+                "FLEX");
         carro.setCor("Vermelho");
-        carro.setMotor(1.4);
+        carro.setMotor("1.4");
         carro.setAcabamento("GLX");
+
         carro.imprimeValores();
+
     }
 }
