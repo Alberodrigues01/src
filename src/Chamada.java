d141503e3d1fe02196d7cf3b1449743d5017311d
Alberto Rodrigues <alberodrigues.filo@outlook.com>
2022-01-06 18:45:25 -0300

    Projeto Chamada - Exercicio Modulo 6

--- src/Chamada.java
+++ src/Chamada.java
@@ -1,43 +1,46 @@
-
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Chamada {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-    Scanner presenca = new Scanner(System.in);
-    Scanner data = new Scanner(System.in);
+        System.out.println("=== DIÁRIO DE CLASSE: Controle de Chamadas ===");
 
-        ArrayList<String> alunos = new ArrayList<>();
-        alunos.add("ANDRÉ");
-        alunos.add("BEATRIZ");
-        alunos.add("CARLOS");
-        alunos.add("DENISE");
+        String[] data = new String[3];
+        data[0] = "04/01/2022";
+        data[1] = "05/01/2022";
+        data[2] = "06/01/2022";
 
-        System.out.println("== Aluna(o)s da Turma ==");
-        System.out.println(alunos);
-        System.out.println("========================");
-        System.out.println("Atenção para a chamada!");
-        System.out.println("Informe a data: ");
-        String dtchamada = data.next();
+        for (int i = 0; i < data.length; i++);
 
-    for (int i = 0; i < alunos.size(); i++) {
-        System.out.print("Aluna(o) => ");
-        System.out.println(alunos.get(i) + " está presente? (informe s/n): ");
-        String resp = presenca.next();
+        String[] aluno = new String[3];
+        aluno[0] = "André";
+        aluno[1] = "Beatriz";
+        aluno[2] = "Carlos";
 
-        if (resp.equals("s")) {
-            System.out.println("Presença registrada.");
-            System.out.println("");
-        } else {
-            System.out.println("Falta registrada.");
+        for (int j = 0; j < aluno.length; j++);
+
+        String[][] chamada = new String[3][3];
+        chamada[0][0] = "presente";
+        chamada[0][1] = "ausente";
+        chamada[0][2] = "presente";
+        chamada[1][0] = "ausente";
+        chamada[1][1] = "ausente";
+        chamada[1][2] = "presente";
+        chamada[2][0] = "presente";
+        chamada[2][1] = "presente";
+        chamada[2][2] = "ausente";
+
+        for (int i = 0; i < chamada.length; i++) {
+            for (int j = 0; j < chamada[i].length; ++j) {
+
+                System.out.println(data[i] + "-" + aluno[j] + "-" + chamada[i][j] + "");
+            }
             System.out.println("");
         }
-      }
-        System.out.println("=======================");
-        System.out.println("Chamada concluída.");
     }
-
 }
+
+
+
 
